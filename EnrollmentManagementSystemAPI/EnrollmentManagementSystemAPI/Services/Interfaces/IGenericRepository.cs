e8df06977e27d366e363376699519d8e651822ee
Ezik143 <jakesucgang90@gmail.com>
2026-05-05 00:09:30 +0800

    fixed some usings

--- EnrollmentManagementSystemAPI/EnrollmentManagementSystemAPI/Services/Interfaces/IGenericRepository.cs
+++ EnrollmentManagementSystemAPI/EnrollmentManagementSystemAPI/Services/Interfaces/IGenericRepository.cs
@@ -1,6 +1,6 @@
 ﻿using System.Linq.Expressions;
 
-namespace EnrollmentManagementSystemAPI.Services
+namespace EnrollmentManagementSystemAPI.Services.Interfaces
 {
     public interface IGenericRepository<T> where T : class
     {
